7b7beb7a968f4aa8dbaee7e1198eee8e8642bc7a
Le Mai Thao Binh <lemaithaobinh1@gmail.com>
2025-03-29 22:01:02 +0700

    Commit update form interaction

---
+++ src/test/java/driver/DriverFactory.java
@@ -0,0 +1,40 @@
+package driver;
+
+import java.time.Duration;
+
+import org.apache.commons.exec.OS;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    public static WebDriver getChromDriver() {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String chromeDriverLocation = "";
+        if (OS.isFamilyMac()) {
+            chromeDriverLocation = currentProjectLocation + "/src/test/resources/drivers/chromedriver";
+        }
+
+        if (OS.isFamilyWindows()) {
+            chromeDriverLocation = currentProjectLocation + "\\src\\test\\resources\\drivers\\chromedriver.exe";
+        }
+
+        if (chromeDriverLocation.isEmpty()) {
+            throw new IllegalArgumentException("Can't detect OS type!");
+        }
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        // Interval time | 500 mili seconds = 0.5 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        return driver;
+
+    }
+}
