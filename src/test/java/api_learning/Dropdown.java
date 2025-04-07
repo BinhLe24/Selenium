5fa105ab92e7e131d2777d4ebf9dd6100995cbf6
Le Mai Thao Binh <lemaithaobinh1@gmail.com>
2025-04-07 23:52:28 +0700

    Lesson 17, iframe

--- src/test/java/api_learning/Dropdown.java
+++ src/test/java/api_learning/Dropdown.java
@@ -3,7 +3,6 @@ package api_learning;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
 import driver.DriverFactory;
 import support.ui.SelectEx;
