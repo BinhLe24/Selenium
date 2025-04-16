1269584fb623ef82d2fb8a9fba21bf315369ceea
Le Mai Thao Binh <lemaithaobinh1@gmail.com>
2025-04-16 23:51:36 +0700

    Lesson 18, dynamic controls

--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -7,5 +7,6 @@ public interface Urls {
     String hoverSlug = "/hovers";
     String jsAlertSlug = "/javascript_alerts";
     String loginSlug = "/login";
+    String dynamicControlSlug = "/dynamic_controls";
 
 }
